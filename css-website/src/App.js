2d85d3e5d1ac47d5eef2b6e64e83caa02fc23401
MOABdev <abmohammed659@gmail.com>
2024-12-14 15:14:04 +0000

    Homepage done

--- css-website/src/App.js
+++ css-website/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
-import About from './pages/about';
-import Services from './pages/services';
-import Contact from './pages/contact';
-import NotFound from './pages/notfound';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles.css";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Services from "./pages/services";
+import Contact from "./pages/contact";
+import NotFound from "./pages/notfound";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <nav>
+      <div className="app">
+        {/* <nav>
           <a href="/">Home</a>
           <a href="/about">About</a>
           <a href="/services">Services</a>
           <a href="/contact">Contact</a>
-        </nav>
+        </nav> */}
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
+          <Route path="/events" element={<About />} />
+          <Route path="/resources" element={<Services />} />
+          <Route path="/members" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
--- css-website/src/pages/home.js
+++ css-website/src/pages/home.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import "../styles/Home.css";
+import LearnConnectGrow from "../components/LearnConnectGrow";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to My Website</h1>
-      <p>This is the Home page.</p>
-    </div>
+    <>
+      <div className="home">
+        <div className="nextEvent"></div>
+        <div className="writings">
+          <div className="societyName">
+            <h1>Computer Science</h1>
+            <p>Society</p>
+          </div>
+          <div className="societyDescription">
+            <p>
+              Go the distance bush league the gloves are off gamble responsible
+              fast and furious daisy-cutter claret on the field
+            </p>
+          </div>
+        </div>
+      </div>
+      <LearnConnectGrow />
+      <Footer />
+    </>
   );
 };
 
